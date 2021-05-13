ce61e8cdabc0835cb42df6a41c722b4ff4b95c25
Arthur Monney <monneylobe@gmail.com>
2021-05-13 17:37:00 +0100

    :sparkles: Ajout de google analytics

--- resources/views/layouts/ga.blade.php
+++ resources/views/layouts/ga.blade.php
@@ -1,5 +1,11 @@
 @production
-    <!-- Google Analytics - beautiful, simple website analytics -->
-    <script src="https://boom.laravel.io/script.js" data-site="UPHUDXHV" defer></script>
-    <!-- / Google Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-J6PRLE9BBY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-J6PRLE9BBY');
+    </script>
 @endproduction
